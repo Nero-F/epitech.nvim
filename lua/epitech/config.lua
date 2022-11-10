56f60b2fed379c98b93595e8c7c204e061145036
Fahad Assoumani <fahad.assoumani@epitech.eu>
2022-11-10 14:06:01 +0100

    add: config management now availiable via setup function
    split header logic to its own lua module

---
+++ lua/epitech/config.lua
@@ -0,0 +1,75 @@
+local config = {}
+local userConfig = {}
+
+local defaultConfig = {
+  header = {
+    headermap = {
+      c = { b = '/*', m= '**', e= '*/' },
+      h = { b = '/*', m= '**', e= '*/' },
+      hpp = { b = '/*', m= '**', e= '*/' },
+      cpp = {b = '//', m = '//', e = '//'},
+      make = {b = '##', m = '##', e = '##'},
+      java = {b = '//', m = '//', e = '//'},
+      latex = {b = '%%', m = '%%', e = '%%'},
+      html = {b = '<!--', m = '  --', e = '-->'},
+      lisp = {b = ';;', m = ';;', e = ';;'},
+      css = {b = '/*', m = '**', e = '*/'},
+      pov = {b = '//', m = '//', e = '//'},
+      pascal = {b = '{ ', m = '   ', e = '}'},
+      haskell = {b = '{-', m = '-- ', e = '-}'},
+      vim = {b = '""', m = '"" ', e = '""'},
+    },
+    com_map_shebang = {
+      sh = {s = '#!/usr/bin/env sh', b = '##', m = '##', e = '##'},
+      bash= {s = '#!/usr/bin/env bash', b = '##', m= '##', e= '##'},
+      zsh= {s = '#!/usr/bin/env zsh', b = '##', m = '##', e = '##'},
+      php= {s = '#!/usr/bin/env php', b = '/*', m = '**', e = '*/'},
+      perl= {s = '#!/usr/bin/env perl', b= '##', m= '##', e = '##'},
+      python= {s = '#!/usr/bin/env python3', b = '##', m= '##', e= '##'},
+      ruby= {s= '#!/usr/bin/env ruby', b= '##', m= '##', e= '##'},
+      node= {s= '#!/usr/bin/env node', b= '/*', m= '**', e= '*/'},
+    }
+  },
+}
+
+local configurableModule = {
+  coding_style = false,
+  diagnostic = false,
+  header = false,
+}
+
+local function require_and_configure(moduleName)
+  local fullName = 'epitech.' .. moduleName
+  local module = require(fullName)
+
+  if not configurableModule[moduleName] and module.cfg then
+    configurableModule[moduleName] = true
+    module.cfg(userConfig[moduleName])
+    return module
+  end
+  return module
+end
+
+local function merge_table(a, b)
+  if type(a) == 'table' and type(b) == 'table' then
+    for k, v in pairs(b) do
+      if type(v) == 'table' and type(a[k] or false) =='table' then
+        merge_table(a[k], v)
+      else
+        a[k] = v
+      end
+    end
+  end
+  return a
+end
+
+function config.set_defaults(userDefaults)
+  if userDefaults == nil then
+    userConfig = defaultConfig
+  else
+    userConfig = merge_table(defaultConfig, userDefaults)
+  end
+  require_and_configure("header")
+end
+
+return config
---
+++ lua/epitech/header.lua
@@ -0,0 +1,124 @@
+local EpiHeader = {}
+local config = nil
+
+EpiHeader.cfg = function(_config)
+  config = require("epitech.utils").cfg(_config)
+end
+
+local getAcademicYear = function ()
+  return 2022
+end
+
+local function append_preproc_directives(ft)
+  local rhs_ext = ft == "hpp" and "_HPP_" or "_H_"
+  local filename = string.upper(vim.fn.expand("%:t:r") .. rhs_ext)
+
+  local cppDirectives = { 
+    define = {
+      "#ifndef " .. filename,
+      "#define " .. filename,
+      "",
+      "#endif /* " .. filename .. " */",
+    },
+    pragma = {
+      "#pragma once"
+    }
+  }
+  if ft == "h" then
+    vim.fn.append(vim.fn.line('.'), cppDirectives.define)
+  else
+    vim.ui.select({"define", "pragma"}, { }, function(choice)
+      vim.fn.append(vim.fn.line('.'), cppDirectives[choice])
+    end)
+  end
+end
+
+local putHeader = function(targetedHeader, fileDesc, projName)
+  local header = {
+    targetedHeader.b,
+    targetedHeader.m .. " EPITECH PROJECT, " .. getAcademicYear(), -- TODO: this need to done 
+    targetedHeader.m .. projName,
+    targetedHeader.m .. " File description:" ,
+    targetedHeader.m .. " " .. fileDesc,
+    targetedHeader.e,
+  }
+
+  vim.fn.append(0, header)
+end
+
+local function get_project_name()
+  local cwd = vim.fn.getcwd()
+  local i, j = string.find(cwd, "/[a-zA-Z0-9 .]+$")
+
+  if i == nil then
+    print("Could nor determine the name of the project..")
+    return nil
+  end
+  return ' ' .. string.sub(cwd, i+1, j)
+end
+
+local function ask_user_input_and_put_header(targetedHeader, ft)
+  local projNameTmp = get_project_name()
+  local defaultString = ""
+
+  if projNameTmp then
+    defaultString = " (default: ".. projNameTmp .. ")  "
+  end
+
+  vim.ui.input("PROJECT NAME?" .. defaultString, function(projName)
+    if projName == nil then
+      projName = projNameTmp
+    else
+      projName = ' ' .. projName
+    end
+    local fileDescTmp = vim.fn.expand("%:t:r")
+    defaultString = " (default:".. fileDescTmp .. ")"
+
+    vim.ui.input("Description" .. defaultString, function(fileDesc)
+      if fileDesc == nil then
+        fileDesc = fileDescTmp
+      end
+      putHeader(targetedHeader, fileDesc, projName)
+      if ft == 'h' or ft == "hpp" then
+        append_preproc_directives(ft)
+      end
+    end)
+  end)
+end
+
+vim.keymap.set('n', '<leader>h', ':EpiHeader<cr>', { noremap=true, silent=false })
+
+vim.api.nvim_create_user_command("EpiHeader", function(_)
+  local ft = vim.fn.expand("%:e")
+  local targetedHeader = config.headermap[ft]
+
+  if targetedHeader == nil then
+    print("Filetype not supported...")
+    return nil
+  end
+
+  ask_user_input_and_put_header(targetedHeader, ft) -- In this order cuz input are non-blocking
+end, { nargs = 0 })
+
+-- WIP
+--[[ local bufn = 6
+local handleResult = function(chanId, data, event)
+  local buffer = vim.fn.readfile("f", "b")
+  vim.api.nvim_buf_set_lines(bufn, 0, -1, false, buffer)
+  local buffString = vim.fn.join(buffer, "")
+  local decoded = vim.json.decode(buffString)
+  P(decoded.id)
+
+end
+
+vim.api.nvim_create_user_command("EpiTestsRun", function(opts)
+  local ret = vim.fn.jobstart("./unit_tests --json=f --always-succeed", {
+    pty=true,
+    stdout_buffered = true,
+    stderr_buffered = true,
+    on_stdout = handleResult,
+    -- on_stderr = doTheJob2, -- for some reason this does not work :/
+  })
+  P(ret)
+end, {}) ]]
+return EpiHeader
